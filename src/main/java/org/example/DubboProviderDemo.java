e3dc2bbca845bcbfa92f23a8b6ba4f4e2e7cc676
Stan <1576302867@qq.com>
2020-04-07 14:27:15 +0800

    init commit

---
+++ src/main/java/org/example/DubboProviderDemo.java
@@ -0,0 +1,9 @@
+package org.example;
+
+@EnableAutoConfiguration
+public class DubboProviderDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderDemo.class,args);
+    }
+}
